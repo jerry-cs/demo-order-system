b4cd6abe2831e9bada8626572493569333d4107c
Jinjie Yang <yangjjie94@gmail.com>
2023-01-07 22:39:09 +0000

    complete Get /orders/{id} API stub

--- src/main/java/dev/jerry/demoordersystem/controller/OrderController.java
+++ src/main/java/dev/jerry/demoordersystem/controller/OrderController.java
@@ -1,11 +1,14 @@
 package dev.jerry.demoordersystem.controller;
 
+import dev.jerry.demoordersystem.exception.OrderNotFoundException;
 import dev.jerry.demoordersystem.model.Mapper;
+import dev.jerry.demoordersystem.model.Order;
 import dev.jerry.demoordersystem.model.OrderDTO;
 import dev.jerry.demoordersystem.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,6 +32,10 @@ public class OrderController {
         return orderService.findAll().stream().map(mapper::toDto).collect(Collectors.toList());
     }
 
-
+    @GetMapping("/{id}")
+    public OrderDTO findById(@PathVariable("id") long id) throws OrderNotFoundException {
+        Order existing = orderService.findById(id).orElseThrow(()-> new OrderNotFoundException("Cannot find Order of id: " + id));
+        return mapper.toDto(existing);
+    }
 
 }
--- src/main/java/dev/jerry/demoordersystem/service/OrderServiceDummyImpl.java
+++ src/main/java/dev/jerry/demoordersystem/service/OrderServiceDummyImpl.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Optional;
 
-
 public class OrderServiceDummyImpl implements OrderService {
 
     private final Collection<Order> orders;
@@ -20,25 +19,30 @@ public class OrderServiceDummyImpl implements OrderService {
 
     private void loadTestData() {
         Item item1 = new Item();
+        item1.setId(1);
         item1.setName("item1");
         item1.setUnitPrice(new BigDecimal("20.54"));
         item1.setQuantity(10);
 
         Item item2 = new Item();
+        item2.setId(2);
         item2.setName("item2");
         item2.setUnitPrice(new BigDecimal("20.54"));
         item2.setQuantity(8);
 
         Item item3 = new Item();
+        item3.setId(3);
         item3.setName("item3");
         item3.setUnitPrice(new BigDecimal("100.21"));
         item3.setQuantity(20);
 
         Order order1 = new Order();
+        order1.setId(1);
         order1.addItem(item1);
         order1.setTotalPrice(new BigDecimal("205.4"));
 
         Order order2 = new Order();
+        order2.setId(2);
         order2.addItem(item2);
         order2.addItem(item3);
         order2.setTotalPrice(new BigDecimal("2168.52"));
@@ -55,7 +59,7 @@ public class OrderServiceDummyImpl implements OrderService {
 
     @Override
     public Optional<Order> findById(long id) {
-        return Optional.empty();
+        return orders.stream().filter( o -> o.getId() == id).findFirst();
     }
 
     @Override
