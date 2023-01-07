d0341bb5650e2fc52e4ce25b1675f2feceb16c2f
Jinjie Yang <yangjjie94@gmail.com>
2023-01-07 19:04:32 +0000

    generate Spring project template
    Dependencies:
    - Spring Web
    - Spring Data JPA
    - Lombok
    - Validation
    - MySQL Driver

---
+++ src/main/java/dev/jerry/demoordersystem/DemoOrderSystemApplication.java
@@ -0,0 +1,13 @@
+package dev.jerry.demoordersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOrderSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoOrderSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/jerry/demoordersystem/DemoOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jerry.demoordersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoOrderSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
